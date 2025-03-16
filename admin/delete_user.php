76d7c3263e8c97dffdff3733003b072f17ae1966
Herald Carl Avila <heavila@my.cspc.edu.ph>
2025-03-16 16:43:44 +0800

    Add user deletion confirmation alert and redirect to user list

--- admin/delete_user.php
+++ admin/delete_user.php
@@ -1,6 +1,13 @@
 <?php
 	require_once 'dbcon.php';
+	session_start();
 	$user_id=$_GET['user_id'];
 	$conn->query("DELETE FROM users WHERE user_id='$user_id'") or die($conn->error);
-	header('location:user.php');
+	?>
+	<script>
+		alert("User has been successfully deleted");
+		window.location = 'user.php';
+	</script>
+	<?php
+	exit();
 ?>
